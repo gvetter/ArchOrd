64bc20e31a004128efc5d1f5375191a2bbdb76fe
FlorianMulhauser <mulhauser.florian@orange.fr>
2017-10-18 10:53:30 +0200

    update PC

--- Lab03/vhdl/PC.vhd
+++ Lab03/vhdl/PC.vhd
@@ -21,12 +21,21 @@ architecture synth of PC is
 begin
 process(clk, reset_n, en, a)
 begin
-if (rising_edge(clk)) then
-		if (en = '1') then 
-			addr <= (31 downto 16 => '0') & std_logic_vector(unsigned(a) + 4);
-		end if;
-		if (reset_n = '1') then
+
+	if (reset_n = '1') then
 			addr <= (31 downto 0 => '0');
+			
+	elsif (rising_edge(clk)) then
+		if (en = '1') then 
+			if((sel_imm or sel_a) = '1') then
+				if (sel_a = '1') then
+					addr <= (31 downto 16 => '0') & a(15 downto 2) & "00";
+				else 
+					addr <= (31 downto 16 => '0') & imm(13 downto 0) & "00";
+				end if;
+			else	
+				addr <= (31 downto 16 => '0') & std_logic_vector(unsigned(a) + 4);
+			end if;
 		end if;
 	end if;
 end process;
